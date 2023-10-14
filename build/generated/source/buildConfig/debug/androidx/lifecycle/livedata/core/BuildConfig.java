db261c1f1370358cceb9ec4336d165bbbabca89c
anas <anasxonerkinjonov@gmail.com>
2023-10-14 10:26:53 +0500

    some change

--- build/generated/source/buildConfig/debug/androidx/lifecycle/livedata/core/BuildConfig.java
+++ build/generated/source/buildConfig/debug/androidx/lifecycle/livedata/core/BuildConfig.java
@@ -4,7 +4,7 @@
 package androidx.lifecycle.livedata.core;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = false;
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "androidx.lifecycle.livedata.core";
   public static final String BUILD_TYPE = "debug";
 }
