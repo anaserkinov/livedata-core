5476178363890832729c2ab6db1210261d7cab5a
anas <anasxonerkinjonov@gmail.com>
2023-12-14 19:06:14 +0500

    some changes

---
+++ build/generated/source/buildConfig/release/androidx/lifecycle/livedata/core/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package androidx.lifecycle.livedata.core;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "androidx.lifecycle.livedata.core";
+  public static final String BUILD_TYPE = "release";
+}
